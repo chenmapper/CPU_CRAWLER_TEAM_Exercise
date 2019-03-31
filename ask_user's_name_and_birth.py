69aba092c8c0d9882b3832f2ff022a9aa98d1667
panxuanyou <dign2ci@gmail.com>
2019-03-31 13:57:47 +0800

    ok

--- ask_user's_name_and_birth.py
+++ ask_user's_name_and_birth.py
@@ -5,14 +5,17 @@
 import re
 
 """ask user's name and birth, then print those on screen"""
+
 now_appliable_year_pattern = r"(19[0-9]{2})|(20[01][0-9])]"
+
+
 def check_birth_format(birth):
-    if re.match(now_appliable_year_pattern,birth):
+    if re.match(now_appliable_year_pattern, birth):
         return int(birth)
     else:
         print("your input not matches the format. Maybe it's not in yyyy format, e.g 1996. Please enter it again!")
-        input_birth = input("please input your birth:")
-        check_birth_format(input_birth)
+        input_birth = input("please input your birth again: ")
+        return check_birth_format(input_birth)
 
 
 input_name = input("please input your name:")
@@ -21,5 +24,6 @@ input_birth = input("please input your birth year:")
 
 correct_input_birth = check_birth_format(input_birth)
 
-print("your name is: %s" %(input_name))
-print("your age is: %d" %correct_input_birth)
+print("your name is: %s" % (input_name.strip()))
+
+print("your age is: %d" % (2020-correct_input_birth))
