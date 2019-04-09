cdcb23e3dddc12bd8a9525cce07058d43ef59207
panxuanyou <dign2ci@gmail.com>
2019-04-09 22:44:18 +0800

    ok

--- divisible_num_list.py
+++ divisible_num_list.py
@@ -5,20 +5,27 @@
 import re
 import time
 
-number = input("請輸入一個數字，將列出可整除這個數字的integer：")
+number_str = input("請輸入一個數字，將列出可整除這個數字的integer：")
 
 
 
 num_pattern = r'^[0-9]+$'
-match = re.match(num_pattern, number.strip())
+match = re.match(num_pattern, number_str.strip())
 
-number_int = int(number)
+number_int = int(number_str)
 
+"""failure. Maybe it is local variable in while. What about variable in if or for?
 if match:
     num = 1
     result_list = []
     while num <= number_int and num%number_int == 0:
         result_list.append(num)
         num += 1
+"""
+"""try to consider variable in whie"""
+if match:
+
+
+
 
 print(result_list)
