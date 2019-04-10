ef75375a80ae9ddb8da376d19b22c4980c98f80b
panxuanyou <dign2ci@gmail.com>
2019-04-10 15:32:50 +0800

    ok

--- guess_num.py
+++ guess_num.py
@@ -2,13 +2,16 @@ import random
 
 
 
-alist = [i for i in range(1,10) if i%2==0]
+
 
 answer_num = random.randint(1,9)
-while num != answer_num:
-    number = int(input("enter:"))
+number = int(input("enter:"))
+while number != answer_num:
     if number < answer_num:
-        number = input('too small, try again:')
-    else if number > answer_num:
-        number = input('too big, try again:')
-    els
+        number = int(input('too small, try again:'))
+    elif number > answer_num:
+        number = int(input('too big, try again:'))
+print('bingo')
+
+for i in range(50):
+    print(random.randint(1,9))
---
+++ is_prime.py
@@ -0,0 +1,56 @@
+#357686312646216567629137
+#        2646216567629137
+import math
+import time
+
+def is_prime(num):
+    if num%2 == 0 and num != 2:
+        print('not a prime number.')
+    else:
+        if num == 1:
+            print('not a prime')
+        else:
+            if num in (2, 3):
+                print('prime')
+            else:
+                flag = 1
+                for n in range(1, int(math.sqrt(num) + 1)): #+1 # prime n^p * m^q * o^r ...
+                    if num % n == 0 and n != 1:
+                        print('not prime')
+                        flag = 0
+                        break
+                if flag:
+                    print('prime')
+
+def is_prime2(num):
+    if num in (0,1):
+        print('not prime')
+    elif num in (2,3):
+        print('prime')
+    else:
+        flag = 1
+        for n in range(2,num): #no +1
+            if num%n == 0:
+                print('not prime')
+                flag = 0
+                break
+        if flag:
+            print('prime')
+
+
+def main():
+    number = input("enter:")
+    print(time.time())
+    is_prime(int(number))
+    print(time.time())
+
+    print('-------------------------')
+
+    number = input("enter:")
+    print(time.time())
+    is_prime2(int(number))
+    print(time.time())
+
+if __name__ == '__main__':
+    main()
+
