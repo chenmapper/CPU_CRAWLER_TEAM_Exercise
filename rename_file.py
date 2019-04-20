06b7b2f4688e6ecef5110bd010031cd110c86074
panxuanyou <dign2ci@gmail.com>
2019-04-20 22:53:47 +0800

    ...

--- rename_file.py
+++ rename_file.py
@@ -9,18 +9,27 @@ import re
 
 directory_path = '/Users/panxuanyou/Downloads/偵查科技/'
 filename_list = listdir(directory_path)
+print(filename_list)
 
 pattern = r'^IMG_[0-9]+'
-matched_filename_list = [re.match(pattern, filename).group() for filename in filename_list]
+#matched_filename_list = [re.match(pattern, filename).group() for filename in filename_list]
+
+matched_filename_list = [filename for filename in filename_list if re.match(pattern, filename)]
+print(matched_filename_list)
 
 pattern2 = r'[0-9]+'
 num_matched_filename_list = [int(re.compile(pattern2).search(elm).group()) for elm in matched_filename_list]
 num_matched_filename_list.sort()
+print(num_matched_filename_list)
 
 extension_string = '.JPG'
 
 filepath_gnr = (directory_path + 'IMG_' + str(filename) + extension_string for filename in num_matched_filename_list)
-filename_of_incremental_num_gnr = (str(i) for i in range(1,len(num_matched_filename_list)))
+filename_of_incremental_num_gnr = (str(i) for i in range(1,len(num_matched_filename_list)+1))
+for p in filepath_gnr:
+    print(p)
+for n in filename_of_incremental_num_gnr:
+    print(n)
 
 
 filepath_and_out_num = [(filepath, out_num) for filepath, out_num in zip(filepath_gnr, filename_of_incremental_num_gnr)]
@@ -39,8 +48,8 @@ def main():
     while execute_anyway_until_there_is_not_file_to_rename:
         try:
             rename_to_incremental_num_from_one()
-        except e(BaseException):
-            print(e)
+        except:
+            break
 
 if __name__ == '__main__':
     main()
